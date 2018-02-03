66608eb5e9f88134322eafd976bb953a415aa132
VirgiliusDagon <36106295+VirgiliusDagon@users.noreply.github.com>
2018-02-04 03:07:27 +0500

    ReversedString added

--- Experimental/StringReverse/StringReverse/StringReverse.cpp
+++ Experimental/StringReverse/StringReverse/StringReverse.cpp
@@ -10,8 +10,19 @@ int main()
 {
 	setlocale(LC_ALL, "rus");
 
+	char str[] = "Hellow World", temp;
+	int i, j;
+	int str_lengh = strlen(str);
+	cout << str << endl;
+	for (i = 0, j = str_lengh - 1 ; i < j; i++, j--) {
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+	}
 
+	cout << str << endl;
 
+	system("pause");
     return 0;
 }
 
