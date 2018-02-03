b3657daaa92e1996acd1715d0c14bcb9df078bcc
VirgiliusDagon <36106295+VirgiliusDagon@users.noreply.github.com>
2018-02-03 23:30:38 +0500

    Commentsuka2

---
+++ StringReverse/StringReverse/StringReverse.cpp
@@ -0,0 +1,17 @@
+// StringReverse.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+
+
+    return 0;
+}
+
